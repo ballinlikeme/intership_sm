5cd7f45cea2700f23b372407524e2c896c75c559
ballinlikeme <maxim.spirkin2003@gmail.com>
2023-05-30 15:29:25 +0300

    fix: removed duplicated wrapper

--- migration/src/App.js
+++ migration/src/App.js
@@ -2,10 +2,8 @@ import { ProjectsPage } from "./pages/ProjectsPage";
 
 function App() {
   return (
-    <div className="wrapper">
       <ProjectsPage />
-    </div>
-  );
+  )
 }
 
 export default App;
