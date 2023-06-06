c4a44a23cf9ab357ec338e39f8712def811d3876
ballinlikeme <maxim.spirkin2003@gmail.com>
2023-06-06 15:39:48 +0300

    feat: eslint

--- migration/src/pages/Projects/Projects.jsx
+++ migration/src/pages/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Layout } from "components/Layout/Layout";
 import { Welcome } from "./Welcome";
-import { Projects as ProjectsList } from "features/projects/components/Projects";
+import { Projects as ProjectsList } from "features/projects";
 
 export const Projects = () => {
   return (
