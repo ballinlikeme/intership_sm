3780ffeb5422ef22acc0c3a08fbf08d6238ec3f1
ballinlikeme <maxim.spirkin2003@gmail.com>
2023-06-08 21:49:42 +0300

    feat: uses cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { router } from "./routers/index.js";
 
 dotenv.config();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
