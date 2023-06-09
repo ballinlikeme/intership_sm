5bf34ece168d23b8044e9babe889322f11016af1
ballinlikeme <maxim.spirkin2003@gmail.com>
2023-06-09 22:47:19 +0300

    feat: justified loader

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./Loader.css";
 
 export const Loader = () => {
-  return <div className="lds-hourglass"></div>;
+  return (
+    <div className="loader">
+      <div className="lds-hourglass"></div>
+    </div>
+  );
 };
