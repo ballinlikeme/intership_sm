4af98929ca00548921092ff440691eb02e69eebd
ballinlikeme <maxim.spirkin2003@gmail.com>
2023-06-12 11:07:22 +0300

    refactor: removed unused method

--- server/src/controllers/ProjectsController.js
+++ server/src/controllers/ProjectsController.js
@@ -1,15 +1,6 @@
 import { projectsService } from "../services/ProjectsService.js";
 
 class ProjectsController {
-  async getProjects(req, res) {
-    try {
-      const projects = await projectsService.getProjects();
-      return res.json(projects);
-    } catch (e) {
-      return res.status(500).json(e);
-    }
-  }
-
   async getProjectsByName(req, res) {
     try {
       const { name } = req.params;
