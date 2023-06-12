08866bb18a9fdd2429222f7ebedea6fdf7faa520
ballinlikeme <maxim.spirkin2003@gmail.com>
2023-06-12 11:28:57 +0300

    fix: husky fixed

--- migration/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Router } from "./lib/router/Router";
-
-function App() {
-  return <Router />;
-}
-
-export default App;
--- migration/src/components/AuthLayout/AuthLayout.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./AuthLayout.css";
-
-export const AuthLayout = ({ children }) => {
-  return <main className="auth__main">{children}</main>;
-};
